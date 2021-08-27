dac0e911eeb6671d2f6b3c5b42a23af40b124d39
Faadhil Ahamat <faadhil.aa@gmail.com>
2021-08-27 00:02:38 +0000

    complemeted full implementation of ISS flyovers

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
+const printPassTimes = function (passTimes){
+  for (let element of passTimes){
+    const duration = element.duration;
+    const date = new Date(0);
+    date.setUTCSeconds(pass.risetime);
+
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
 
-  console.log('It worked! Returned IP:', ip);
+}
+
+
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
+
+
+
+
+
+
+//EARLY IMPLEMENTED CODE
+
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   fetchCoordsByIP(ip, (error, data) => {
+//     if (error) {
+//       console.log("It didn't work!", error);
+//       return;
+//     }else{
+//       fetchISSFlyOverTimes(data, (error, data) =>{
+//         if (error) {
+//           console.log("It didn't work!", error);
+//           return;
+//         }else{
+//           console.log('It worked! Returned flyover times:', data)
+//         }
+//       })
+//       console.log(data);
+//     }
+//   })
+//   console.log('It worked! Returned IP:', ip);
+// });
+  
+
+
+
+
+
--- iss.js
+++ iss.js
@@ -19,4 +19,64 @@ const fetchMyIP = function (callback) {
 
 }
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function (ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    const data = JSON.parse(body);
+
+    if (error || data.ip === undefined) {
+      callback(error, null);
+      return
+    } else if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    } else {
+      callback(null, {latitude: data.latitude, longitude: data.longitude});
+    }
+  })
+
+}
+
+
+const fetchISSFlyOverTimes = function (coords, callback) {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    console.log(data)
+
+    if (error || data.ip === undefined) {
+      callback(error, null);
+      return
+    } else if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    } else {
+      callback(null, data);
+    }
+  })
+
+}
+
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
+  });
+};
+
+module.exports = { fetchMyIP , fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation};
